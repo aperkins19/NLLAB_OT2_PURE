ed4aad7da9de97f6392fdbc850c1bc17501fd6ce
Alex P from nllab_ot2 <a.j.p.perkins@sms.ed.ac.uk>
2022-09-26 17:53:13 +0100

    Added component master mix compliations. Still ned to add a mix step.

--- src/OT2_scripts/OP1/OP1_technical_error_AL_pipetting_script.py
+++ src/OT2_scripts/OP1/OP1_technical_error_AL_pipetting_script.py
@@ -3,12 +3,12 @@ import json
 
 # metadata
 metadata = {
-    "protocolName": "PURE CFPS Plating Script v1",
+    "protocolName": "OP1 plating v1",
     "author": "Alex Perkins",
 
     "email": "a.j.p.perkins@sms.ed.ac.uk",
     "description": "First draft of script to plate out 3x PURE rxns",
-    "apiLevel": "2.3",
+    "apiLevel": "2.5",
 }
 
 # Add 35ul of lysate to a PCR tube to A1 in cold block
@@ -57,46 +57,100 @@ def run(protocol: protocol_api.ProtocolContext):
     # labware
 
     # Defining the temperature module
-    temperature_module = protocol.load_module(labware_settings_dict["temp_module_name"], labware_settings_dict["temp_module_pos"])
+    temperature_module = protocol.load_module(labware_settings_dict["temp_module"]["name"], labware_settings_dict["temp_module"]["deck_position"])
 
 
     # Defining the pcr plate ontop of the temperature module
     # https://www.bio-rad.com/en-uk/sku/hsp9601-hard-shell-96-well-pcr-plates-low-profile-thin-wall-skirted-white-clear?ID=hsp9601
     pcr_temp_plate = temperature_module.load_labware(
-        labware_settings_dict["pcr_temp_plate_name"],
+        labware_settings_dict["pcr_temp_plate"]["name"],
         label="Temperature-Controlled Tubes",
     )
 
+    protocol.comment(str(labware_settings_dict["nunc_384"]["offsets"]["x"]))
+
+    pcr_temp_plate.set_offset(x = labware_settings_dict["pcr_temp_plate"]["offsets"]["x"],
+                              y = labware_settings_dict["pcr_temp_plate"]["offsets"]["y"],
+                              z = labware_settings_dict["pcr_temp_plate"]["offsets"]["z"])
+
     # Defining the 384 nunc well plate
-    nunc_384 = protocol.load_labware(labware_settings_dict["nunc_384_name"], labware_settings_dict["nunc_384_pos"])
+    nunc_384 = protocol.load_labware(labware_settings_dict["nunc_384"]["name"], labware_settings_dict["nunc_384"]["deck_position"])
+
+    nunc_384.set_offset(x = labware_settings_dict["nunc_384"]["offsets"]["x"],
+                                  y = labware_settings_dict["nunc_384"]["offsets"]["y"],
+                                  z = labware_settings_dict["nunc_384"]["offsets"]["z"])
+
 
     # Defining the 1.5ul eppendorf rack
     eppendorf_1500ul_x24_rack = protocol.load_labware(
-        labware_settings_dict["eppendorf_1500ul_x24_rack_name"], labware_settings_dict["eppendorf_1500ul_x24_rack_pos"]
+        labware_settings_dict["eppendorf_1500ul_x24_rack"]["name"], labware_settings_dict["eppendorf_1500ul_x24_rack"]["deck_position"]
     )
 
+    eppendorf_1500ul_x24_rack.set_offset(x = labware_settings_dict["eppendorf_1500ul_x24_rack"]["offsets"]["x"],
+                                  y = labware_settings_dict["eppendorf_1500ul_x24_rack"]["offsets"]["y"],
+                                  z = labware_settings_dict["eppendorf_1500ul_x24_rack"]["offsets"]["z"])
+
+
+
     # Defining the 20ul tip rack
-    tiprack_20ul = protocol.load_labware(labware_settings_dict["tiprack_20ul_name"], labware_settings_dict["tiprack_20ul_pos"])
+    tiprack_20ul = protocol.load_labware(labware_settings_dict["tiprack_20ul"]["name"], labware_settings_dict["tiprack_20ul"]["deck_position"])
+
+    tiprack_20ul.set_offset(x = labware_settings_dict["tiprack_20ul"]["offsets"]["x"],
+                                  y = labware_settings_dict["tiprack_20ul"]["offsets"]["y"],
+                                  z = labware_settings_dict["tiprack_20ul"]["offsets"]["z"])
+
 
     # Defining left_pipette (p20)
     left_pipette = protocol.load_instrument(
-        labware_settings_dict["left_pipette_name"], "left", tip_racks=[tiprack_20ul]
+        labware_settings_dict["left_pipette"]["name"], "left", tip_racks=[tiprack_20ul]
     )
 
     # Defining the 300ul tip rack
-    tiprack_300ul = protocol.load_labware(labware_settings_dict["tiprack_300ul_name"], labware_settings_dict["tiprack_300ul_pos"])
+    tiprack_300ul = protocol.load_labware(labware_settings_dict["tiprack_300ul"]["name"], labware_settings_dict["tiprack_300ul"]["deck_position"])
+
+    tiprack_300ul.set_offset(x = labware_settings_dict["tiprack_300ul"]["offsets"]["x"],
+                                  y = labware_settings_dict["tiprack_300ul"]["offsets"]["y"],
+                                  z = labware_settings_dict["tiprack_300ul"]["offsets"]["z"])
+
 
     # Defining right_pipette (p300)
     right_pipette = protocol.load_instrument(
-        labware_settings_dict["right_pipette_name"], "right", tip_racks=[tiprack_300ul]
+        labware_settings_dict["right_pipette"]["name"], "right", tip_racks=[tiprack_300ul]
     )
 
     # 2. Defining functions used in this protocol------------------------------
+
+    # distributing_components_to_master_mix_from_stock
+    def distributing_components_to_master_mix_from_stock(well, components_source_well, lysate_aspirate_height):
+
+        left_pipette.pick_up_tip()
+
+
+        left_pipette.well_bottom_clearance.aspirate = lysate_aspirate_height
+        left_pipette.well_bottom_clearance.dispense = pipetting_settings_dict["lysate_dispense_well_bottom_clearance"]
+
+        # aspirate step
+        left_pipette.aspirate(pipetting_settings_dict["lysate_aspirate_volume"], pcr_temp_plate[components_source_well], rate=pipetting_settings_dict["lysate_aspirate_rate"])
+        left_pipette.move_to(pcr_temp_plate[components_source_well].top(-2))
+        protocol.delay(seconds=2)
+        left_pipette.touch_tip()
+
+        left_pipette.move_to(pcr_temp_plate[components_source_well].top(10))
+
+        # Dispense Step
+        left_pipette.dispense(pipetting_settings_dict["lysate_dispense_volume"], pcr_temp_plate[well], rate=pipetting_settings_dict["lysate_dispense_rate"])
+        left_pipette.touch_tip()
+
+        left_pipette.move_to(pcr_temp_plate[well].top(10))
+
+        left_pipette.drop_tip()
+
 
     # adding_and_mixing_ribosomes_with_components
     def adding_and_mixing_ribosomes_with_components(well, ribosomes_source_well, ribosome_aspirate_height):
 
         left_pipette.pick_up_tip()
+
 
         left_pipette.well_bottom_clearance.aspirate = ribosome_aspirate_height
         left_pipette.well_bottom_clearance.dispense = pipetting_settings_dict["lysate_dispense_well_bottom_clearance"]
@@ -107,9 +161,13 @@ def run(protocol: protocol_api.ProtocolContext):
         protocol.delay(seconds=2)
         left_pipette.touch_tip()
 
+        left_pipette.move_to(pcr_temp_plate[ribosomes_source_well].top(10))
+
         # Dispense Step
         left_pipette.dispense(pipetting_settings_dict["ribosomes_dispense_volume"], pcr_temp_plate[well], rate=pipetting_settings_dict["ribosomes_dispense_rate"])
         left_pipette.touch_tip()
+
+        left_pipette.move_to(pcr_temp_plate[well].top(10))
 
         left_pipette.drop_tip()
 
@@ -192,27 +250,79 @@ def run(protocol: protocol_api.ProtocolContext):
     experiment_ids = experiment_settings_dict.keys()
 
     # Master Mix Compilation
-    protocol.comment("Adding and mixing ribosomes with components...")
-
-    # Components + Ribosomes
-
-    ribosome_aspirate_height = pipetting_settings_dict["ribosomes_aspirate_height_inc"]
-
-    for exp in experiment_settings_dict:
-
-        components_master_mix_well = experiment_settings_dict[exp]["lysate_source_well"]
-        ribosomes_source_well = reagent_sources_dict["Components"]["Supplimentry_Components"]["Ribosomes"]["Well"]
-        protocol.comment(ribosomes_source_well)
-        adding_and_mixing_ribosomes_with_components(components_master_mix_well, ribosomes_source_well, ribosome_aspirate_height)
+    protocol.comment("Beginning Master Mix Compilations...")
 
 
+    # Components distribution
+
+    # toggle for components distribution
+    components_distribution_step_toggle = True
+
+    if components_distribution_step_toggle:
+
+        protocol.comment("Distributing components from stock to master mix wells...")
+
+        #initialise ribosome_aspirate_height
+        lysate_aspirate_height = pipetting_settings_dict["lysate_aspirate_height_init"]
+
+        # loop over the experiments settings dict to get the lysate source wells
+        # which are the component master mixes for each experiment
+        for exp in experiment_settings_dict:
+
+            # set the master mix well
+            components_master_mix_well = experiment_settings_dict[exp]["lysate_source_well"]
+
+            # look up where the master stock of components are
+            components_source_well = reagent_sources_dict["Components"]["Base_System"]["Well"]
+
+            protocol.comment(components_source_well)
+
+            distributing_components_to_master_mix_from_stock(components_master_mix_well, components_source_well, lysate_aspirate_height)
+
+
+        protocol.comment("Distributing components complete.")
 
 
 
 
+    # Ribosome Addition
+
+    # toggle for ribosome adding_and_mixing_ribosomes_with_components
+    ribosome_mixing_step_toggle = False
+
+    if ribosome_mixing_step_toggle:
+
+        protocol.comment("Adding and mixing ribosomes with components...")
+
+        #initialise ribosome_aspirate_height
+        ribosome_aspirate_height = pipetting_settings_dict["ribosomes_aspirate_height_init"]
+
+        # loop over the experiments settings dict to get the lysate source wells
+        # which are the component master mixes for each experiment
+        for exp in experiment_settings_dict:
+
+            # set the master mix well
+            components_master_mix_well = experiment_settings_dict[exp]["lysate_source_well"]
+
+            # look up where the master stock of ribosomes are
+            ribosomes_source_well = reagent_sources_dict["Components"]["Supplimentry_Components"]["Ribosomes"]["Well"]
+
+            protocol.comment(ribosomes_source_well)
+
+            adding_and_mixing_ribosomes_with_components(components_master_mix_well, ribosomes_source_well, ribosome_aspirate_height)
+
+
+        protocol.comment("Ribosome mix step complete.")
+
+
+
+    protocol.comment(" ")
+    protocol.comment("Master Mix compliations complete.")
 
     # Running the substrate dispense step if protocol_dispense_subsrates = True
     if protocol_dispense_subsrates:
+
+        protocol.comment("Beginning substrates plating step...")
 
         # Looping through the different experiments
         for experiment_id in experiment_ids:
